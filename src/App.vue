87913fcb5e82b5a2720ee963596aae1218fc08b3
Nikhil Mara <nikhil.mara1999@gmail.com>
2024-09-30 20:24:26 +0530

    formated code and removed startup files

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,12 @@
 <script setup lang="ts">
+import { useCounterStore } from './stores/counter'
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+const store = useCounterStore()
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    {{ store.count }}
+    <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
