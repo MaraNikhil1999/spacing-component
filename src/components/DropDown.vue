435e00d5558e528313595c126a3e9b2c2a090ee4
Nikhil Mara <nikhil.mara1999@gmail.com>
2024-10-02 22:22:43 +0530

    applied closing of the dropdown when cliked outside the dropbox

--- src/components/DropDown.vue
+++ src/components/DropDown.vue
@@ -6,24 +6,40 @@ interface Props {
 }
 const props = defineProps<Props>()
 const showDropDown = ref(false)
+const mainElement = ref<HTMLElement>()
 const emit = defineEmits(['set-values'])
 
 function setInputValueInParent(DropDownOption: any) {
-    showDropDown.value = false
     emit('set-values', DropDownOption)
+    toggleDropDown()
+}
+function toggleDropDown() {
+    showDropDown.value = !showDropDown.value
+    if (showDropDown.value) {
+        document.addEventListener('click', checkIsOutSideClick)
+    } else {
+        document.removeEventListener('click', checkIsOutSideClick)
+    }
+}
+
+function checkIsOutSideClick(event: MouseEvent) {
+    const target = event.target as HTMLElement
+    if (target && !mainElement.value?.contains(target)) {
+        toggleDropDown()
+    }
 }
 </script>
 
 <template>
-    <div>
-        <button @click="showDropDown = !showDropDown">
-            <img class="icon" src="@/icons/icons.svg" alt="Icon" />
+    <div ref="mainElement">
+        <button @click="toggleDropDown" class="no-bg-bdr cursor-pointer">
+            <img class="icon" :class="{ 'rotate': showDropDown }" src="@/icons/icons.svg" alt="Icon" />
         </button>
-        <div v-if="showDropDown" class="pos-abs bg-white">
-            <ul class="list-ctnr">
+        <div v-if="showDropDown" class="pos-rel">
+            <ul class="list-ctnr bg-white brdr-rad-1 box-shd pos-abs">
                 <li
                     v-for="option in props.options"
-                    :key="option.key"
+                    :key="option.value"
                     class="cursor-pointer list"
                     @click="setInputValueInParent(option)"
                 >
@@ -37,20 +53,37 @@ function setInputValueInParent(DropDownOption: any) {
 <style scoped>
 .icon {
     width: calc(var(--width-unit) * 2);
-    height: 10px;
+    height: 100%;
 }
 
 .cursor-pointer {
     cursor: pointer;
 }
+.rotate{
+    transform: rotate(180deg);
+}
+
+.box-shd {
+    box-shadow: 0px 0px 12px 6px #b7b5c1;
+}
 
 .list-ctnr {
-    padding: calc(var(--padding-unit) * 2);
+    padding: calc(var(--padding-unit) * 1);
     margin: 0;
+    width: max-content;
+    left: -74px;
+    top: calc(var(--height-unit) * 2);
+    z-index: 1;
+    text-align: start;
+    font-weight: 600;
 }
 
 .list {
     list-style-type: none;
-    margin: 10px 0;
+    padding: calc(var(--margin-unit) * 2) calc(var(--margin-unit) * 4);
+}
+
+.list:hover {
+    background-color: var(--hover-color-grey);
 }
 </style>
