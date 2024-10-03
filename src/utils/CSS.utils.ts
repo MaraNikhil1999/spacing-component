3a169d2c5b0656e61b126b216d2d474a1354e734
Nikhil Mara <nikhil.mara1999@gmail.com>
2024-10-03 14:34:31 +0530

    renamed function as per its functionality

--- src/utils/CSS.utils.ts
+++ src/utils/CSS.utils.ts
@@ -1,6 +1,11 @@
 import type { SpacingProperty } from '../Interface/StyleObj'
 
-export const isValidPadding = (input: string) => {
+/**
+ * 
+ * @param input input value to check for padding or margin
+ * @returns true if the input is a valid padding or margin value else false
+ */
+export const isValidInputValue = (input: string) => {
     const paddingRegex =
         /^(\d+(px|em|rem|%|vh)\s*)|(auto|inherit|initial|revert|revert-layey|unset)$/
     return paddingRegex.test(input)
